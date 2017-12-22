f23ed51cfa4503fc575e4a416c6033aa2e08ba97
Søren Bjergmark <sahb1239@gmail.com>
2017-12-23 00:07:04 +0100

    Removed not needed using statements

--- src/SAHB.GraphQLClient/QueryBuilder/GraphQLQueryBuilder.cs
+++ src/SAHB.GraphQLClient/QueryBuilder/GraphQLQueryBuilder.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using Newtonsoft.Json;
 using SAHB.GraphQLClient.FieldBuilder;
 
 namespace SAHB.GraphQLClient.QueryBuilder
