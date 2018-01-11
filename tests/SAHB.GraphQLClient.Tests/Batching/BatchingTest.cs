2fb6d9701efffadbde3346bf6123e7bcc1b8f9d7
Søren Bjergmark <sahb1239@gmail.com>
2018-01-11 12:19:29 +0100

    Test case for conflicting arguments

--- tests/SAHB.GraphQLClient.Tests/Batching/BatchingTest.cs
+++ tests/SAHB.GraphQLClient.Tests/Batching/BatchingTest.cs
@@ -17,7 +17,8 @@ namespace SAHB.GraphQLClient.Tests.Batching
         public async Task Test_Combine_Two_Simple_Queries()
         {
             // Arrange
-            var expected = "{\"query\":\"query{batch0_Part1Field1:part1_field1 batch0_Part1Field2:part1Field2 batch1_Part2Field3:part2_field3 batch1_Part2Field4:part2Field4}\"}";
+            var expected =
+                "{\"query\":\"query{batch0_Part1Field1:part1_field1 batch0_Part1Field2:part1Field2 batch1_Part2Field3:part2_field3 batch1_Part2Field4:part2Field4}\"}";
             var httpClientMock = new GraphQLHttpExecutorMock(
                 JsonConvert.SerializeObject(new
                 {
@@ -29,7 +30,8 @@ namespace SAHB.GraphQLClient.Tests.Batching
                         batch1_Part2Field4 = "Value4"
                     }
                 }), expected);
-            var client = new GraphQLHttpClient(httpClientMock, new GraphQLFieldBuilder(), new GraphQLQueryGeneratorFromFields());
+            var client = new GraphQLHttpClient(httpClientMock, new GraphQLFieldBuilder(),
+                new GraphQLQueryGeneratorFromFields());
 
             // Act
             var batch = client.CreateBatch("");
@@ -51,6 +53,7 @@ namespace SAHB.GraphQLClient.Tests.Batching
         {
             [GraphQLFieldName("part1_field1")]
             public string Part1Field1 { get; set; }
+
             public string Part1Field2 { get; set; }
         }
 
@@ -58,6 +61,115 @@ namespace SAHB.GraphQLClient.Tests.Batching
         {
             [GraphQLFieldName("part2_field3")]
             public string Part2Field3 { get; set; }
+
+            public string Part2Field4 { get; set; }
+        }
+
+        [Fact]
+        public async Task Test_Combine_With_Arguments()
+        {
+            // Arrange
+            var expected =
+                @"{""query"":""query($argumentVariable:Int $argumentVariable2:Int){batch0_Part1Field1:part1_field1(argumentName:$argumentVariable) batch0_Part1Field2:part1Field2 batch1_Part2Field3:part2_field3(argumentName:$argumentVariable2) batch1_Part2Field4:part2Field4}"",""Variables"":""{\""argumentVariable\"":\""1\"",\""argumentVariable2\"":\""2\""}""}";
+            var httpClientMock = new GraphQLHttpExecutorMock(
+                JsonConvert.SerializeObject(new
+                {
+                    Data = new
+                    {
+                        batch0_Part1Field1 = "Value1",
+                        batch0_Part1Field2 = "Value2",
+                        batch1_Part2Field3 = "Value3",
+                        batch1_Part2Field4 = "Value4"
+                    }
+                }), expected);
+            var client = new GraphQLHttpClient(httpClientMock, new GraphQLFieldBuilder(),
+                new GraphQLQueryGeneratorFromFields());
+
+            // Act
+            var batch = client.CreateBatch("");
+            var query1 =
+                batch.Query<QueryBatchWithArgumentsPart1>(new GraphQLQueryArgument("argumentVariable", 1.ToString()));
+            var query2 =
+                batch.Query<QueryBatchWithArgumentsPart2>(new GraphQLQueryArgument("argumentVariable2", 2.ToString()));
+
+            var result1 = await query1.Execute();
+            var result2 = await query2.Execute();
+
+            // Assert
+            Assert.Equal(result1.Part1Field1, "Value1");
+            Assert.Equal(result1.Part1Field2, "Value2");
+
+            Assert.Equal(result2.Part2Field3, "Value3");
+            Assert.Equal(result2.Part2Field4, "Value4");
+        }
+
+        public class QueryBatchWithArgumentsPart1
+        {
+            [GraphQLFieldName("part1_field1"), GraphQLArguments("argumentName", "Int", "argumentVariable")]
+            public string Part1Field1 { get; set; }
+
+            public string Part1Field2 { get; set; }
+        }
+
+        public class QueryBatchWithArgumentsPart2
+        {
+            [GraphQLFieldName("part2_field3"), GraphQLArguments("argumentName", "Int", "argumentVariable2")]
+            public string Part2Field3 { get; set; }
+
+            public string Part2Field4 { get; set; }
+        }
+
+        [Fact]
+        public async Task Test_Combine_With_Conflicting_Arguments()
+        {
+            // Arrange
+            var expected =
+                @"{""query"":""query($argumentVariable:Int $argumentVariable:Int){batch0_Part1Field1:part1_field1(argumentName:$argumentVariable) batch0_Part1Field2:part1Field2 batch1_Part2Field3:part2_field3(argumentName:$argumentVariable2) batch1_Part2Field4:part2Field4}"",""Variables"":""{\""argumentVariable\"":\""1\"",\""argumentVariable2\"":\""2\""}""}";
+            var httpClientMock = new GraphQLHttpExecutorMock(
+                JsonConvert.SerializeObject(new
+                {
+                    Data = new
+                    {
+                        batch0_Part1Field1 = "Value1",
+                        batch0_Part1Field2 = "Value2",
+                        batch1_Part2Field3 = "Value3",
+                        batch1_Part2Field4 = "Value4"
+                    }
+                }), expected);
+            var client = new GraphQLHttpClient(httpClientMock, new GraphQLFieldBuilder(),
+                new GraphQLQueryGeneratorFromFields());
+
+            // Act
+            var batch = client.CreateBatch("");
+            var query1 =
+                batch.Query<QueryBatchWithConflictingArgumentsPart1>(new GraphQLQueryArgument("argumentVariable", 1.ToString()));
+            var query2 =
+                batch.Query<QueryBatchWithConflictingArgumentsPart2>(new GraphQLQueryArgument("argumentVariable", 2.ToString()));
+
+            var result1 = await query1.Execute();
+            var result2 = await query2.Execute();
+
+            // Assert
+            Assert.Equal(result1.Part1Field1, "Value1");
+            Assert.Equal(result1.Part1Field2, "Value2");
+
+            Assert.Equal(result2.Part2Field3, "Value3");
+            Assert.Equal(result2.Part2Field4, "Value4");
+        }
+
+        public class QueryBatchWithConflictingArgumentsPart1
+        {
+            [GraphQLFieldName("part1_field1"), GraphQLArguments("argumentName", "Int", "argumentVariable")]
+            public string Part1Field1 { get; set; }
+
+            public string Part1Field2 { get; set; }
+        }
+
+        public class QueryBatchWithConflictingArgumentsPart2
+        {
+            [GraphQLFieldName("part2_field3"), GraphQLArguments("argumentName", "Int", "argumentVariable")]
+            public string Part2Field3 { get; set; }
+
             public string Part2Field4 { get; set; }
         }
     }
