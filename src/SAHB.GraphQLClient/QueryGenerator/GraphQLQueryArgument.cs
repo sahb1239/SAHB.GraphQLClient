3794cad9c7c4657cac1910904fc2172f160f6d8c
Søren Bjergmark <sahb1239@gmail.com>
2018-01-30 17:31:20 +0100

    Changed argumentValue to be type object to support complex argument values

--- src/SAHB.GraphQLClient/QueryGenerator/GraphQLQueryArgument.cs
+++ src/SAHB.GraphQLClient/QueryGenerator/GraphQLQueryArgument.cs
@@ -14,7 +14,7 @@ namespace SAHB.GraphQLClient.QueryGenerator
         /// </summary>
         /// <param name="variableName">The variable name which should be set used in the <see cref="GraphQLArgumentsAttribute"/></param>
         /// <param name="argumentValue">The value which is inserted in the variables part of the GraphQL query</param>
-        public GraphQLQueryArgument(string variableName, string argumentValue)
+        public GraphQLQueryArgument(string variableName, object argumentValue)
         {
             VariableName = variableName ?? throw new ArgumentNullException(nameof(variableName));
             ArgumentValue = argumentValue ?? throw new ArgumentNullException(nameof(argumentValue));
@@ -28,6 +28,6 @@ namespace SAHB.GraphQLClient.QueryGenerator
         /// <summary>
         /// The value which is inserted in the variables part of the GraphQL query
         /// </summary>
-        public string ArgumentValue { get; set; }
+        public object ArgumentValue { get; set; }
     }
 }
