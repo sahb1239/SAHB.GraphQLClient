e8836a48b1a8a0772efc5f1713ab88ac5f5df568
Søren Bjergmark <sahb1239@gmail.com>
2018-01-30 22:28:47 +0100

    Added Arguments required exception

---
+++ src/SAHB.GraphQLClient/Exceptions/GraphQLArgumentsRequiredException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAHB.GraphQLClient.FieldBuilder;
+
+namespace SAHB.GraphQLClient.Exceptions
+{
+    public class GraphQLArgumentsRequiredException : GraphQLException
+    {
+        public GraphQLArgumentsRequiredException(IEnumerable<GraphQLFieldArguments> arguments) : base("The arguments:\n" + string.Join(", ", arguments.Select(argument => argument.VariableName + ":" + argument.ArgumentName)) + " is required")
+        {
+            Arguments = arguments;
+        }
+
+        public IEnumerable<GraphQLFieldArguments> Arguments { get; }
+    }
+}
