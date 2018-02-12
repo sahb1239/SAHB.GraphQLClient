707a5531a5b5935d3518c8a5f650648ce2fb87a1
Søren Bjergmark <sahb1239@gmail.com>
2018-02-12 23:16:25 +0100

    Added docs

--- src/SAHB.GraphQLClient/Exceptions/GraphQLArgumentsRequiredException.cs
+++ src/SAHB.GraphQLClient/Exceptions/GraphQLArgumentsRequiredException.cs
@@ -6,6 +6,10 @@ using SAHB.GraphQLClient.FieldBuilder;
 
 namespace SAHB.GraphQLClient.Exceptions
 {
+    // ReSharper disable once InconsistentNaming
+    /// <summary> 
+    /// <see cref="GraphQLArgumentsRequiredException"/> is thrown when a required argument is not supplied for generating the query
+    /// </summary> 
     public class GraphQLArgumentsRequiredException : GraphQLException
     {
         public GraphQLArgumentsRequiredException(IEnumerable<GraphQLFieldArguments> arguments) : base("The arguments:\n" + string.Join(", ", arguments.Select(argument => argument.VariableName + ":" + argument.ArgumentName)) + " is required")
