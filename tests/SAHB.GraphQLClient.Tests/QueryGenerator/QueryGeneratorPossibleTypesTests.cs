7c31c5c1af95735e6a913610a1a6cfbd5b475686
Søren Bjergmark <sahb1239@gmail.com>
2018-05-21 22:54:57 +0200

    Extra tests

--- tests/SAHB.GraphQLClient.Tests/QueryGenerator/QueryGeneratorPossibleTypesTests.cs
+++ tests/SAHB.GraphQLClient.Tests/QueryGenerator/QueryGeneratorPossibleTypesTests.cs
@@ -32,11 +32,80 @@ namespace SAHB.GraphQL.Client.Tests.QueryGenerator
         {
             var fields = new[]
             {
-                new GraphQLField("alias", "field", null, null, new []{new GraphQLPossibleType(new List<GraphQLField> { new GraphQLField("alias", "field", null, null, null), new GraphQLField("alias2", "field2", null, null, null) }, "interfaceConcreteType" )}),
+                new GraphQLField("alias", "field", null, null,
+                    new[]
+                    {
+                        new GraphQLPossibleType(
+                            new List<GraphQLField>
+                            {
+                                new GraphQLField("alias", "field", null, null, null),
+                                new GraphQLField("alias2", "field2", null, null, null)
+                            }, "interfaceConcreteType")
+                    }),
             };
             var fieldBuilder = new FieldBuilderMock(fields);
             var queryGenerator = new GraphQLQueryGeneratorFromFields();
             var expected = "{\"query\":\"query{alias:field ... on interfaceConcreteType{alias:field alias2:field2}}\"}";
+
+            var actual = queryGenerator.GetQuery<string>(fieldBuilder); // Typeparameter is ignored since it just returns the fields
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_Multiple_Other_Possible_Type_With_Same_Field()
+        {
+            var fields = new[]
+            {
+                new GraphQLField("alias", "field", null, null,
+                    new[]
+                    {
+                        new GraphQLPossibleType(
+                            new List<GraphQLField>
+                            {
+                                new GraphQLField("alias", "field", null, null, null)
+                            }, "interfaceConcreteType1"),
+                        new GraphQLPossibleType(
+                            new List<GraphQLField>
+                            {
+                                new GraphQLField("alias", "field", null, null, null)
+                            }, "interfaceConcreteType2")
+                    }),
+            };
+            var fieldBuilder = new FieldBuilderMock(fields);
+            var queryGenerator = new GraphQLQueryGeneratorFromFields();
+            var expected = "{\"query\":\"query{alias:field ... on interfaceConcreteType1{alias:field} ... on interfaceConcreteType2{alias:field}}\"}";
+
+            var actual = queryGenerator.GetQuery<string>(fieldBuilder); // Typeparameter is ignored since it just returns the fields
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Test_Multiple_Other_Possible_Type_With_Extra_Field()
+        {
+            var fields = new[]
+            {
+                new GraphQLField("alias", "field", null, null,
+                    new[]
+                    {
+                        new GraphQLPossibleType(
+                            new List<GraphQLField>
+                            {
+                                new GraphQLField("alias", "field", null, null, null),
+                                new GraphQLField("alias2", "field2", null, null, null)
+                            }, "interfaceConcreteType1"),
+                        new GraphQLPossibleType(
+                            new List<GraphQLField>
+                            {
+                                new GraphQLField("alias", "field", null, null, null),
+                                new GraphQLField("alias2", "field2", null, null, null)
+                            }, "interfaceConcreteType2")
+                    }),
+            };
+            var fieldBuilder = new FieldBuilderMock(fields);
+            var queryGenerator = new GraphQLQueryGeneratorFromFields();
+            var expected = "{\"query\":\"query{alias:field ... on interfaceConcreteType1{alias:field alias2:field2} ... on interfaceConcreteType2{alias:field alias2:field2}}\"}";
 
             var actual = queryGenerator.GetQuery<string>(fieldBuilder); // Typeparameter is ignored since it just returns the fields
 
