c6432e0e7392de89411173edf7badbfcd57076c2
Søren Bjergmark <sahb1239@gmail.com>
2018-09-05 21:13:41 +0200

    Added testcases for issue #61

---
+++ tests/SAHB.GraphQLClient.Tests/Issues/Issue61.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using SAHB.GraphQLClient;
+using SAHB.GraphQLClient.FieldBuilder;
+using SAHB.GraphQLClient.QueryGenerator;
+using Xunit;
+
+namespace SAHB.GraphQL.Client.Tests.Issues
+{
+    public class Issue61
+    {
+        [Fact]
+        public void GraphQLQueryGeneratorFromFields_Should_Throw_When_No_Matching_Argument_Found()
+        {
+            // Arrange
+            var fieldGenerator = new GraphQLFieldBuilder();
+            var queryGenerator = new GraphQLQueryGeneratorFromFields();
+            
+            // Act / Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Get the query
+                var query = queryGenerator.GetQuery(fieldGenerator.GetFields(typeof(SampleQuery)),
+                    new GraphQLQueryArgument("helloArgument", "Value"));
+            });
+        }
+
+        [Fact]
+        public void Client_Should_Throw_When_No_Matching_Argument_Found()
+        {
+            // Arrange
+            var client = GraphQLHttpClient.Default();
+
+            // Act / Assert
+            Assert.Throws<Exception>(() =>
+            {
+                // Get the query
+                var query = client.CreateQuery< SampleQuery>("url", arguments:
+                    new GraphQLQueryArgument("helloVariableName", "helloVariableValue"));
+            });
+        }
+
+
+        public class SampleQuery
+        {
+            public string Hello { get; set; }
+        }
+    }
+}
