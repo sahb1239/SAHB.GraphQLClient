1831179f5eead4a78bdd7e3149ef5955953e3fc9
Søren Bjergmark <sahb1239@gmail.com>
2018-09-05 21:38:35 +0200

    Added more tests for issue #61

--- tests/SAHB.GraphQLClient.Tests/Issues/Issue61.cs
+++ tests/SAHB.GraphQLClient.Tests/Issues/Issue61.cs
@@ -6,6 +6,7 @@ using System.Text;
 using SAHB.GraphQLClient;
 using SAHB.GraphQLClient.Exceptions;
 using SAHB.GraphQLClient.FieldBuilder;
+using SAHB.GraphQLClient.FieldBuilder.Attributes;
 using SAHB.GraphQLClient.QueryGenerator;
 using Xunit;
 
@@ -38,6 +39,34 @@ namespace SAHB.GraphQL.Client.Tests.Issues
         }
 
         [Fact]
+        public void GraphQLQueryGeneratorFromFields_Should_Throw_When_No_Matching_Argument_Found_Two_Arguments()
+        {
+            // Arrange
+            var fieldGenerator = new GraphQLFieldBuilder();
+            var queryGenerator = new GraphQLQueryGeneratorFromFields();
+
+            var argument1 = new GraphQLQueryArgument("hello1Argument", "Value1");
+            var argument2 = new GraphQLQueryArgument("hello2Argument", "Value2");
+
+            // Act / Assert
+            var exception = Assert.Throws<GraphQLArgumentVariableNotFoundException>(() =>
+            {
+                // Get the query
+                var query = queryGenerator.GetQuery(fieldGenerator.GetFields(typeof(SampleQuery)),
+                    argument1, argument2);
+            });
+
+            // Assert
+            // Should contain two element
+            Assert.Equal(2, exception.Arguments.Count());
+
+            // Argument should contain helloArgument and value
+            Assert.Contains(argument1, exception.Arguments);
+            Assert.Contains(argument2, exception.Arguments);
+        }
+
+
+        [Fact]
         public void Client_Should_Throw_When_No_Matching_Argument_Found()
         {
             // Arrange
@@ -64,6 +93,37 @@ namespace SAHB.GraphQL.Client.Tests.Issues
         public class SampleQuery
         {
             public string Hello { get; set; }
+        }
+
+        [Fact]
+        public void GraphQLQueryGeneratorFromFields_Should_Throw_When_No_Matching_Argument_Found_And_Only_One_Of_2_Arguments_Found()
+        {
+            // Arrange
+            var fieldGenerator = new GraphQLFieldBuilder();
+            var queryGenerator = new GraphQLQueryGeneratorFromFields();
+
+            // Act / Assert
+            var exception = Assert.Throws<GraphQLArgumentVariableNotFoundException>(() =>
+            {
+                // Get the query
+                var query = queryGenerator.GetQuery(fieldGenerator.GetFields(typeof(SampleQuery2)),
+                    new GraphQLQueryArgument("hello1Argument", "Value1"), new GraphQLQueryArgument("hello2Argument", "Value2"));
+            });
+
+            // Assert
+            // Should contain one element
+            Assert.Equal(1, exception.Arguments.Count());
+
+            // Argument should contain helloArgument and value
+            Assert.Equal("hello2Argument", exception.Arguments.First().VariableName);
+            Assert.Equal("Value2", exception.Arguments.First().ArgumentValue);
+        }
+
+        public class SampleQuery2
+        {
+            [GraphQLArguments("hello1Argument", "String", "hello1Argument")]
+            public string Hello1 { get; set; }
+            public string Hello2 { get; set; }
         }
     }
 }
