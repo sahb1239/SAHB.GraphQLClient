49ac504c35609c3b17f6d6cef307a378f1afb225
Jeroen Pot <jeroen.pot@snappet.org>
2019-01-09 10:41:00 +0100

    Fix a nullreference error when an access denied message is given without any location.

---
+++ tests/SAHB.GraphQLClient.Tests/Exceptions/GraphQLErrorExceptionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SAHB.GraphQLClient.Exceptions;
+using SAHB.GraphQLClient.Result;
+using Xunit;
+
+namespace SAHB.GraphQLClient.Tests.Exceptions
+{
+    
+    public class GraphQLErrorExceptionTests
+    {
+        [Fact]
+        public void Get_Message_Should_Not_Throw_Exception()
+        {
+            var errorList = new List<GraphQLDataError> { new GraphQLDataError { Message = "Access denied." } };
+
+            new GraphQLErrorException("query", errorList);
+        }
+    }
+}
