efc6dd8a3425224f3a79aa88344ef1a5dc842651
Søren Bjergmark <sahb1239@gmail.com>
2019-05-26 17:21:16 +0200

    Added extra testcases for batch

--- tests/SAHB.GraphQL.Client.Integration.Tests/TestMutation.cs
+++ tests/SAHB.GraphQL.Client.Integration.Tests/TestMutation.cs
@@ -3,6 +3,7 @@ using SAHB.GraphQLClient.Integration.Tests.TestServer;
 using SAHB.GraphQLClient;
 using System.Threading.Tasks;
 using Xunit;
+using SAHB.GraphQLClient.FieldBuilder;
 
 namespace SAHB.GraphQLClient.Integration.Tests
 {
@@ -27,6 +28,26 @@ namespace SAHB.GraphQLClient.Integration.Tests
 
             // Assert
             Assert.Equal("mutation", result.Hello);
+        }
+
+        [Fact]
+        public async Task TestBatchMutation()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var graphQLClient = GraphQLHttpClient.Default(client);
+
+            // Act
+            var batch = graphQLClient.CreateBatch(GraphQLOperationType.Mutation, "http://localhost/graphql");
+            var query1 = batch.Query<TestHelloMutation>();
+            var query2 = batch.Query<TestHelloMutation>();
+
+            var result1 = await query1.Execute();
+            var result2 = await query2.Execute();
+
+            // Assert
+            Assert.Equal("mutation", result1.Hello);
+            Assert.Equal("mutation", result2.Hello);
         }
 
         public class TestSchema : Schema
--- tests/SAHB.GraphQL.Client.Integration.Tests/TestQuery.cs
+++ tests/SAHB.GraphQL.Client.Integration.Tests/TestQuery.cs
@@ -3,6 +3,7 @@ using SAHB.GraphQLClient.Integration.Tests.TestServer;
 using SAHB.GraphQLClient;
 using System.Threading.Tasks;
 using Xunit;
+using SAHB.GraphQLClient.FieldBuilder;
 
 namespace SAHB.GraphQLClient.Integration.Tests
 {
@@ -23,10 +24,30 @@ namespace SAHB.GraphQLClient.Integration.Tests
             var graphQLClient = GraphQLHttpClient.Default(client);
 
             // Act
-            var result = await graphQLClient.CreateQuery<TestHelloQuery>("http://localhost/graphql").Execute();
+            var result = await graphQLClient.Execute<TestHelloQuery>(GraphQLOperationType.Query, "http://localhost/graphql");
 
             // Assert
             Assert.Equal("query", result.Hello);
+        }
+
+        [Fact]
+        public async Task TestBatchQuery()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var graphQLClient = GraphQLHttpClient.Default(client);
+
+            // Act
+            var batch = graphQLClient.CreateBatch(GraphQLOperationType.Query, "http://localhost/graphql");
+            var query1 = batch.Query<TestHelloQuery>();
+            var query2 = batch.Query<TestHelloQuery>();
+
+            var result1 = await query1.Execute();
+            var result2 = await query2.Execute();
+
+            // Assert
+            Assert.Equal("query", result1.Hello);
+            Assert.Equal("query", result2.Hello);
         }
 
         public class TestSchema : Schema
